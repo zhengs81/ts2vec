362f6a8b3456a5e4ed772ed8212337542576dc94
Shaoxuan Zheng <shaoxuan.zheng@mail.utoronto.ca>
2023-07-11 15:39:21 +0800

    finish eval

--- validate/knn_plot.py
+++ validate/knn_plot.py
@@ -1,7 +1,8 @@
 import os, sys, torch, argparse
-from argparse import Namespace
 from torch.utils.data import DataLoader
-from argparse import Namespace
+import numpy as np
+from sklearn.neighbors import KDTree
+import matplotlib.pyplot as plt
 # Set path to SHAPE
 current = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.dirname(current))
@@ -10,7 +11,7 @@ from utils.load_data import load_test_data
 from dataset.timeseries_dataset import TimeSeriesTestDataset
 
 def load_model(path, gpu, params):
-    model = Pretrained(params, [4,8])
+    model = Pretrained(params, [2**i for i in range(1, 11)])
     if gpu:
         model.load_state_dict(torch.load(path, map_location=lambda storage, loc: storage.cuda(gpu)))
     else:
@@ -18,6 +19,19 @@ def load_model(path, gpu, params):
     model.eval()
     return model
 
+def avgpool_timestamp_output(output):
+    """
+    Given output from encoder, this function performs average pool to squeeze T dim
+    output: [B, T, hidden]
+    """
+    return np.average(output, axis=-1)
+
+def maxpool_timestamp_output(output):
+    """
+    Given output from encoder, this function performs average pool to squeeze T dim
+    output: [B, T, hidden]
+    """
+    return np.amax(output, axis=-1)
 
 
 if __name__ == '__main__':
@@ -28,6 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('--stride', default=72, type=int)
     parser.add_argument('--seq_length', default=144, type=int, help='GPU index to use, if CUDA is enabled')
     parser.add_argument('--batch_size', default=128, type=int, help='Size of batches used for splitting the test data to avoid out of memory errors when using CUDA.')
+    parser.add_argument('--num_neighbours', default=5, type=int, help='number of closet neighbours to find, including itself')
     args = parser.parse_args()
 
     args.device = torch.device('cuda' if args.gpu and torch.cuda.is_available() else 'cpu')
@@ -36,14 +51,53 @@ if __name__ == '__main__':
     dataset = TimeSeriesTestDataset(data, args.seq_length, args.stride) 
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
 
-    params = torch.load("../saved_models/pretrained_model_param.pt")
+    params = torch.load("../saved_models/pretrained_model_params.pt")
     model = load_model(args.model, args.gpu, params)
 
-    for metadata, batch in dataloader: # batch [128, 144, 1]
-        # metadata is list of 2 tensors
-        input = batch.unsqueeze(-1).to(args.device)
-        output = model.net(input) # output [128, 64, 8]
+    dataset_index = np.array([]) # index of the dataset at where the query is located
+    slice_index = np.array([]) # index within the dataset, query length is args.seq_length
+    encoded_vec = np.zeros((1, args.seq_length)) # to store encoded vec, shape of [total_batch, hidden_dim]
 
+    with torch.no_grad():
+        for metadata, batch in dataloader: # batch [128, 144, 1]
+            # metadata is list of 2 tensors with each tensor to be B(128) length
+            dataset_index = np.append(dataset_index, metadata[0]) # notice after append, still 1D array
+            slice_index = np.append(slice_index, metadata[1])
+            input = batch.unsqueeze(-1).to(args.device)
+            output = model.net(input) # output [B, T, hidden]
+            squeezed_out = avgpool_timestamp_output(output) # [B, T], or maxpool_timestamp_output
+            encoded_vec = np.append(encoded_vec, squeezed_out, axis=0)
 
+    encoded_vec = encoded_vec[1:] # remove the first zero-row
+    
+    # KD tree 
+    tree = KDTree(encoded_vec)
+    random_test_idx = np.random.randint(0, encoded_vec.shape[0]) # which query to test
+    dist, ind = tree.query(encoded_vec[random_test_idx: random_test_idx+1], k = args.num_neighbours)
+    ind = ind[0]
+    
+    neighbors_seq = []
 
-    encoded_rep = model.encode_window(data, args.window_len, args.batch_size)
+    for i in ind:
+        d, s = int(dataset_index[i]), int(slice_index[i])
+        neighbors_seq.append(data[d][s: s+args.seq_length])
+
+    # now neighbors_seq is a length k list, with each element being length args.seq_length array
+
+    # plot
+    space = int(0.3 * args.seq_length)
+
+    x_axis = [list(range(i * (args.seq_length + space), i * (args.seq_length + space) + args.seq_length)) for i in range(args.num_neighbours)]
+
+    fig, ax = plt.subplots()
+
+    for i in range(args.num_neighbours):
+        ax.plot(x_axis[i], neighbors_seq[i], label='Test Sequence' if not i else 'Closet neighbour # '+str(i))
+
+    # Set labels and legend
+    ax.set_xlabel("tested sequence with its " + str(args.num_neighbours) + " closet neighbours")
+    ax.set_ylabel('value')
+    ax.legend()
+
+    # Show the plot
+    plt.show()
